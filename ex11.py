c828ba43e802e4fd9d0054a31c0ae231beeedc15
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-08-29 15:40:13 -0300

    ex11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+# · Dada a quilometragem parcial de um carro e a quantidade de litros gastos ele para percorrer esta quilometragem, fazer um algoritmo que calcule quantos Km/l o carro percorreu.
+
+km = int(input("Informe a kilometragem percorrida: "))
+litros = int(input("Informe os litros consumidos: "))
+
+kmPl = km/litros
+
+print("o carro fez", kmPl, "Km/l")
