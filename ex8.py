52e3615b92a1598909c30f71adbc6944a016ac1f
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-08-29 15:32:25 -0300

    ex8

---
+++ ex8.py
@@ -0,0 +1,9 @@
+# · Tendo três valores inteiros (representados pelas variáveis A, B e C), calcule e apresente como resultado final o valor da soma dos quadrados dos três valores lidos.
+
+a = int(input("Informe um valor inteiro: "))
+b = int(input("Informe outro valor inteiro: "))
+c = int(input("Informe outro valor inteiro: "))
+
+r = a**2 + b**2 + c**2
+
+print(r)
